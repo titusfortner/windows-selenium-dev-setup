3c998b4e8cb3035fff913c5e56be7eb56d56a047
Titus <titusfortner@users.noreply.github.com>
2015-09-15 10:36:24 -0700

    prep Windows install to run tests

--- go.cmd
+++ go.cmd
@@ -5,9 +5,26 @@ choco install -y python2
 choco install -y ruby
 choco install -y jdk7
 choco install -y dotnet3.5
+choco install -y firefox
+choco install -y phantomjs
+choco install -y google-chrome-x64
+choco install -y curl
 
 REM .NET Framework 4.0 is a prerequisite for chocolatey,
 REM so is unneeded as a separate install.
 REM choco install -y dotnet4.0
 
 if [%1]==[addvs] choco install -y visualstudio2015community -packageParameters "--Features MDDCPlusPlus"
+
+set CHROMEDRIVER_CMD=curl -s http://chromedriver.storage.googleapis.com/LATEST_RELEASE
+%CHROMEDRIVER_CMD% > temp.txt
+set /p CHROMDERIVER_VERSION=<temp.txt
+
+curl -L -O "http://chromedriver.storage.googleapis.com/%CHROMEDRIVER_VERSION%/chromedriver_win32.zip"
+
+jar xf chromedriver_win32.zip
+mkdir C:\drivers
+move chromedriver.exe drivers
+setx path “%path%C:\drivers”
+
+git clone —depth 1 https://github.com/SeleniumHQ/selenium.git
