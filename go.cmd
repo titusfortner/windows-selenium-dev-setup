6b75c448f77ff5c2edda7f2c5d29ff0c8970c747
Titus <titusfortner@users.noreply.github.com>
2015-09-15 11:12:32 -0700

    Add wires and edge drivers

--- go.cmd
+++ go.cmd
@@ -16,15 +16,26 @@ REM choco install -y dotnet4.0
 
 if [%1]==[addvs] choco install -y visualstudio2015community -packageParameters "--Features MDDCPlusPlus"
 
-set CHROMEDRIVER_CMD=curl -s http://chromedriver.storage.googleapis.com/LATEST_RELEASE
-%CHROMEDRIVER_CMD% > temp.txt
+curl -L -O "http://download.microsoft.com/download/8/D/0/8D0D08CF-790D-4586-B726-C6469A9ED49C/MicrosoftWebDriver.msi"
+msiexec MicrosoftWebDriver.msi /qb TARGETDIR=C:\drivers
+move “C:\drivers\Microsoft Web Driver\MicrosoftWebDriver.exe” C:\drivers
+del C:\drivers\MicrosoftWebDriver.msi
+rmdir “C:\drivers\Microsoft Web Driver”
+
+curl -s http://chromedriver.storage.googleapis.com/LATEST_RELEASE > temp.txt
 set /p CHROMDERIVER_VERSION=<temp.txt
-
 curl -L -O "http://chromedriver.storage.googleapis.com/%CHROMEDRIVER_VERSION%/chromedriver_win32.zip"
-
 jar xf chromedriver_win32.zip
-mkdir C:\drivers
-move chromedriver.exe drivers
+move chromedriver.exe C:\drivers
+
+curl -L -O "https://github.com/jgraham/wires/releases/download/0.3.0/wires-0.3.0-windows.zip"
+jar xf wires-0.3.0-windows.zip
+move wires.exe C:\drivers
+
+curl -L -O "http://selenium-release.storage.googleapis.com/2.47/IEDriverServer_x64_2.47.0.zip"
+
 setx path “%path%C:\drivers”
 
 git clone —depth 1 https://github.com/SeleniumHQ/selenium.git
+
+pause > nul
